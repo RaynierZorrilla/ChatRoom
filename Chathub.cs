3fc01934e2a517fc8984205c8d3849279685357f
Raynier Zorrilla <zorrillaraynier2003@gmail.com>
2024-04-06 23:29:13 -0400

    Agregar archivos de proyecto.

---
+++ Chathub.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+
+namespace ChatRoom
+{
+	public class Chathub : Hub
+	{
+		public async Task SendMessage( string room, string user, string message)
+		{
+			//Enviamos un mensaje asincronico a todos los integrantes de la sala
+			await Clients.Group(room).SendAsync("ReceiveMessage", user, message);
+		}
+
+		public async Task AddToGroup(string room)
+		{
+			//Añadir persona a la sala
+			await Groups.AddToGroupAsync(Context.ConnectionId, room);
+			await Clients.Group(room).SendAsync("ShowWho",
+				$"Se Conectó {Context.ConnectionId}");
+		}
+	}
+}
---
+++ Controllers/ChatController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ChatRoom.Controllers
+{
+	public class ChatController : Controller
+	{
+		public static Dictionary<int, string> Rooms =
+				new Dictionary<int, string>()
+				{
+					{ 1, "Sala 1 - Dudas Nómina"},
+					{ 2, "Sala 2 - FAQ"},
+					{ 3, "Sala 3 - Soporte Técnico"},
+					{ 4, "Sala 4 - Quejas o Sugerencias"}
+				};
+
+		public IActionResult RoomList()
+		{
+			return View();
+		}
+
+		public IActionResult Room(int room)
+		{
+			return View("Room", room);
+		}
+	}
+}
